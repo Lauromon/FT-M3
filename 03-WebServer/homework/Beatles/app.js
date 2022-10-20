af05bc02aee6b2931af3849e0f7a9a44d245fe0a
Lautaro <orbesle@gmail.com>
2022-10-20 10:41:07 -0300

    todo Ok

--- 03-WebServer/homework/Beatles/app.js
+++ 03-WebServer/homework/Beatles/app.js
@@ -22,3 +22,53 @@ var beatles=[{
   profilePic:"http://cp91279.biography.com/BIO_Bio-Shorts_0_Ringo-Starr_SF_HD_768x432-16x9.jpg"
 }
 ]
+
+http.createServer( function(req, res){ 
+	if( req.url === '/'){
+		res.writeHead(200, { 'Content-Type':'text/html' })
+		var html = fs.readFileSync(__dirname +'/index.html');
+		return res.end(html);
+	}
+  else if(req.url === '/api'){
+		res.writeHead(200, { 'Content-Type':'application/json' })
+		return res.end( JSON.stringify(beatles) );
+
+  }
+  else if(req.url.substring(0,5) === '/api/'){
+    const beatle = req.url.split('/').pop()
+    const found = beatles.find((b)=> encodeURI(b.name) === beatle)
+    if(found){
+      res.writeHead(200, { 'Content-Type':'application/json' })
+      res.end( JSON.stringify(found) );
+    }
+    res.writeHead(404, { 'Content-Type':'text/plain' })
+		return res.end('no ta monancio')
+  }
+  else if(req.url.length > 1){
+    const beatle = req.url.split('/').pop()
+    const found = beatles.find((b)=> encodeURI(b.name) === beatle)
+    if(found){
+      fs.readFile('./beatle.html', 'utf8', function(err, data){
+      if(err){
+        res.writeHead(404, { 'Content-Type':'text/plain' })
+        return res.end('no ta monancio')
+      }
+      data = data.replace('{name}', found.name)
+      data = data.replace('{birthdate}', found.birthdate)
+      data = data.replace('{profilePic}', found.profilePic)
+
+      res.writeHead(200, { 'Content-Type':'text/html' })
+      res.end(data);
+      })
+    }
+    else{
+      res.writeHead(404, { 'Content-Type':'text/plain' })
+          return res.end('no ta monancio')
+    }
+  }
+  else{
+    res.writeHead(404, { 'Content-Type':'text/plain' })
+        return res.end('no ta monancio')
+  }
+  
+}).listen(1337, '127.0.0.1');
