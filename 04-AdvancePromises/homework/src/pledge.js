5acbdcd860079819457cb8efce357ef2d241ace4
Lautaro <orbesle@gmail.com>
2022-10-21 12:14:08 -0300

    todo OK

--- 04-AdvancePromises/homework/src/pledge.js
+++ 04-AdvancePromises/homework/src/pledge.js
@@ -6,20 +6,20 @@ Promises Workshop: construye la libreria de ES6 promises, pledge.js
 
 function $Promise (executor){
     this._state = 'pending';
-    this._value ;
-    var esto = this;
-    var resolve = function(data){
-        return esto._internalResolve(data);
-    };
-    var reject = function(data){
-        return esto._internalReject(data);
-    };
-
+    this._handlerGroups = [];
+    
     if(typeof executor !== 'function'){
         throw TypeError('El arg executor tiene que ser function')
     };
-    
-    executor(resolve,reject);
+    /* var resolve = function(data){
+        return this._internalResolve(data).bind(this);
+    };
+    var reject = function(data){
+        return this._internalReject(data).bind(this);
+    }; */
+
+
+    executor(this._internalResolve.bind(this),this._internalReject.bind(this));
 };
 
 
@@ -27,14 +27,38 @@ $Promise.prototype._internalResolve = function(data){
     if(this._state === 'pending'){
         this._state = 'fulfilled',
         this._value = data
+        this._callHandlers()
     };
 };
+
 $Promise.prototype._internalReject = function(data){
     if(this._state === 'pending'){
         this._state = 'rejected',
         this._value = data
+        this._callHandlers()
     };
-}
+};
+
+$Promise.prototype.then = function(successCb,errorCb){
+    if(typeof successCb !== 'function'){ successCb = false }
+    if(typeof errorCb !== 'function'){ errorCb = false}
+    this._handlerGroups.push({successCb,errorCb})
+    if(this._state !=='pending'){
+        this._callHandlers()
+    }
+};
+
+$Promise.prototype._callHandlers = function(){
+    while(this._handlerGroups.length){
+        var handlf = this._handlerGroups.shift()
+        if (this._state ==='fulfilled'){
+            handlf.successCb && handlf.successCb(this._value);
+        }
+        else{
+            handlf.errorCb && handlf.errorCb(this._value);
+        }
+    }
+};
 
 module.exports = $Promise;
 /*-------------------------------------------------------
